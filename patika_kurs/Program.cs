972f0e29d2c41b1c42b9d73c3cbc2f2eae8a8134
tugce <tugce@DESKTOP-AJHTESO>
2022-01-02 20:45:59 +0300

    Proje dosyası ekle.

---
+++ patika_kurs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
